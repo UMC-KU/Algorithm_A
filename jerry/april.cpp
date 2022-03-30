16523f1f4085d0d7f3fe65ed3838242460e3a8be
pepperdad <jeong20909@naver.com>
2022-03-30 11:58:32 +0900

    0330 jerry / setting

---
+++ jerry/april.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <string>
+#include <queue>
+#include <cstring>
+#include <tuple>
+#include <cmath>
+#include <math.h>
+#define MAX_N 10+5
+#define endl "\n"
+#define INF 2147483647
+typedef long long ll;
+//typedef pair<int, int> pii;
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout << "Hi";
+  
+}
