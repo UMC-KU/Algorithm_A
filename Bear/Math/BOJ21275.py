f0a61b23babce63bcdbc5cd045f2d4423980e96e
jungwoo <rlawjddn103@naver.com>
2022-04-01 03:22:13 +0900

    Fix Math BOJ21275

--- Bear/Math/BOJ21275.py
+++ Bear/Math/BOJ21275.py
@@ -2,7 +2,7 @@ import sys
 
 A, B = sys.stdin.readline().rstrip().split()
 x, a, b = 0, 0, 0 
-count = 0 
+cnt = 0 
 
 
 
@@ -16,16 +16,16 @@ for i in range(2, 37):
         try:
             tempB = int(B, j)
             if tempA == tempB:
-                count += 1
+                cnt += 1
                 x = tempA
                 a = i
                 b = j
         except:
             continue
 
-if count == 1:
+if cnt == 1:
     print(x,a,b)
-elif count == 0:
+elif cnt == 0:
     print("Impossible")
 else:
     print("Multiple")
