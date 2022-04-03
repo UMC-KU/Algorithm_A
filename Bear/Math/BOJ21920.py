a972647f7ba97bdf2f1e2928ed36f3334a0607f6
jungwoo <rlawjddn103@naver.com>
2022-04-03 23:39:50 +0900

    Add Math BOJ3029

--- Bear/Math/BOJ21920.py
+++ Bear/Math/BOJ21920.py
@@ -3,16 +3,15 @@ import sys
 
 N = int(sys.stdin.readline().rstrip())
 A = list(map(int,sys.stdin.readline().rstrip().split()))
-B = set(A)
 X = int(sys.stdin.readline().rstrip())
 
 sum = 0
 cnt = 0
 
-for item in B:
-    if item != X:
-        if gcd(item,X) == 1:
-            sum += item
-            cnt += 1
+for item in A:
+    
+    if gcd(item,X) == 1:
+        sum += item
+        cnt += 1
 
 print(sum/cnt)
