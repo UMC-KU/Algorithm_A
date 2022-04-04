ffc429eb3209a3c77313d9e36d3bcee146b50e4d
ro-el-c <chung3286@naver.com>
2022-04-04 23:37:27 +0900

    add Math BOJ 2609

---
+++ ro-el/Math/BJ_2609.py
@@ -0,0 +1,13 @@
+# 최대공약수와 최소공배수
+
+def GCD(a, b):
+    if b == 0:
+        return a
+    return GCD(b, a % b)
+
+
+a, b = map(int, input().split())
+
+gcd = GCD(max(a, b), min(a, b))
+print(gcd)
+print(a*b//gcd)
